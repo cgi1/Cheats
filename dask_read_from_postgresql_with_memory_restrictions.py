6379363fffc68bb356ce13bb0c62b4636ce0239e
cgi1 <gies0r@gmail.com>
2020-02-02 17:09:44 +0100

    Update dask_read_from_postgresql_with_memory_restrictions.py

--- dask_read_from_postgresql_with_memory_restrictions.py
+++ dask_read_from_postgresql_with_memory_restrictions.py
@@ -1,11 +1,12 @@
 import dask.dataframe as dd
 
+
         approx_MB_per_chunk = 256
         divisions = list('abcdefghijklmnopqrstuvwz')
 
         bytes_per_chunk = approx_MB_per_chunk * 2 ** 20
-        print("Memory consumbtion will be around (%s GB; %s MB per chunk on %s chunks)" % (
-            (approx_MB_per_chunk * len(divisions)), approx_MB_per_chunk, len(divisions)
+        print("Memory consumptions will be around %.2f GB; %.0f MB per chunk on %.0f chunks." % (
+            (approx_MB_per_chunk * len(divisions))/1024, approx_MB_per_chunk, len(divisions)
         ))
         df = dd.read_sql_table(table=tbl_name,
                                uri=self.db.get_create_engine_string(), # This returns an sqlalchemy create engine scheme
@@ -13,3 +14,7 @@ import dask.dataframe as dd
                                divisions=divisions,
                                columns=columns,
                                bytes_per_chunk=bytes_per_chunk)
+
+        print("Finished read_sql_table... (%s)" % tbl_name)
+        df = df.set_index('timestamp')  # set the index to make some operations fast
+        print("Finished set_index... (%s)" % 'timestamp')
