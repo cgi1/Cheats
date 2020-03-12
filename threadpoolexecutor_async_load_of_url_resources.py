94e92216b7f93caee5cbf566f481fcc9c9c2ac8e
cgi1 <gies0r@gmail.com>
2020-03-12 23:21:17 +0100

    Update threadpoolexecutor_async_load_of_url_resources.py

--- threadpoolexecutor_async_load_of_url_resources.py
+++ threadpoolexecutor_async_load_of_url_resources.py
@@ -2,18 +2,20 @@ from concurrent.futures import ThreadPoolExecutor
 
 class AsyncThreadPoolGetter():
 
-  def ___init___():
+  def ___init___(self):
     self.loaded_data = pd.DataFrame()
 
-  def request_from_url(url)
+  def request_from_url(self, url)
       print("Do your request here and save the results to a class variable using self.loaded_data")
 
-  with ThreadPoolExecutor(max_workers=max_threads) as executor:
+  def run_urls(self, urls)
+    
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
 
-      futures_to_todo = []
-      for url in urls:
-          futures_to_todo.append(executor.submit(fn=request_from_url, domain=url))
+        futures_to_todo = []
+        for url in urls:
+            futures_to_todo.append(executor.submit(fn=request_from_url, domain=url))
 
-      wait(futures_to_todo)
-      print("Finished async job.")
+        wait(futures_to_todo)
+        print("Finished async job.")
 
