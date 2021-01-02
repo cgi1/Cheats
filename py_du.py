8855d2bdde22e0e54ad69353e49f029b91e3151f
Christoph Giese <gies0r@gmail.com>
2021-01-02 23:25:33 +0100

    Delete py_du.py

--- py_du.py
+++
@@ -1,9 +0,0 @@
-#Orig from https://stackoverflow.com/a/25574638/940592
-
-def du(path):
-    try:
-        """disk usage in human readable format (e.g. '2,1GB')"""
-        ret_val = subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
-        return int(ret_val)
-    except:
-        print(f"Error> Could du folder ({path}) {fe()}")
